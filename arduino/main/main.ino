a8cb48d22522c93844f95516292b70efc427481e
Sebnahme <71684691+Sebnahme@users.noreply.github.com>
2024-04-16 08:47:44 -0400

    Added gripper code

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -71,7 +71,7 @@ void setup() {
   
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  //myservo.attach(11);
+  myservo.attach(13);
   // motors.calibrate();
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -138,7 +138,16 @@ void loop() {
   
     }
     else if (multi > 1950 && multi <= 2000) {
-
+      cntrl.altitude_hold(true);
+      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        //delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        //delay(15);                       // waits 15ms for the servo to reach the position
+      }
     }
     else{
       cntrl.altitude_hold(false);
